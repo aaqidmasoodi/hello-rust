70d4011ca918b31b63899725025c2cf9096eb00e
aaqidmasoodi <aaqidmasoodi@icloud.com>
2024-06-19 22:30:07 +0530

    introducting variables

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-use std::io;
-use rand::Rng;
-
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
