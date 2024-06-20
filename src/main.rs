cf8164113689d353e51b8597599734b26f633aff
aaqidmasoodi <aaqidmasoodi@icloud.com>
2024-06-20 14:44:54 +0530

    guessing game with loop

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-use std::io;    
+use std::{cmp::Ordering, io};    
+use rand::Rng;
+
 
 fn main() {
 
-
-    let mut name = String::new();
-
-    println!("Hello, What is your name? ");
-    io::stdin()
-        .read_line(&mut name)
-        .expect("error reading input");
+    let correct_answer = rand::thread_rng().gen_range(1..=10);
+    println!("Correct Answer: {correct_answer}");
 
 
-    println!("Hello {}, Welcome to rust.", name);
+    loop {
+
+        let mut guess = String::new();
+
+        println!("Guess a number: 1-10 ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("error reading input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num, 
+            Err(e) => {
+                println!("Error with parsing, {e}");
+                continue;
+            }
+        };
+
+        let message = match guess.cmp(&correct_answer) {
+            Ordering::Greater => "Too High",
+            Ordering::Less => "Too Low",
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            },
+        };
+
+        println!("{message}");
+    }
+    
 }
