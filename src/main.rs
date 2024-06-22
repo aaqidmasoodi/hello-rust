f9b30a4f44ce4a65f8995a7c05ea694af5c261f3
aaqidmasoodi <aaqidmasoodi@icloud.com>
2024-06-23 01:25:47 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,5 @@ use rand::Rng;
 
 fn main() {
 
-    let correct_answer = rand::thread_rng().gen_range(1..=10);
-    println!("Correct Answer: {correct_answer}");
-
-
-    loop {
-
-        let mut guess = String::new();
-
-        println!("Guess a number: 1-10 ");
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("error reading input");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num, 
-            Err(e) => {
-                println!("Error with parsing, {e}");
-                continue;
-            }
-        };
-
-        let message = match guess.cmp(&correct_answer) {
-            Ordering::Greater => "Too High",
-            Ordering::Less => "Too Low",
-            Ordering::Equal => {
-                println!("Correct!");
-                break;
-            },
-        };
-
-        println!("{message}");
-    }
     
 }
